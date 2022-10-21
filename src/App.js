90f65b91f201ac35582db8bc6d507b0be222e1ab
mitchell nacion <mbnacion@optimum.net>
2022-10-21 02:21:51 -0400

    incorporate data from data.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import TravelLocation from './components/TravelLocation';
+import {default as Data} from './data';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header>
-        <img src="/images/globe-icon.svg" alt=""></img>my travel journal
+        <img src="/images/globe-icon.svg" alt=""></img>my travel journal.
       </header>
-      <TravelLocation
-        imgSource="https://source.unsplash.com/WLxQvbMyfas"
-        country="Japan"
-        locationLink="#"
-        title="Mount Fuji"
-        dates="12 Jan, 2021 - 24 Jan, 2021"
-        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+      {Data.map((data,i)=>{
+        return <TravelLocation
+        imgSource={data.imageUrl}
+        country={data.location}
+        locationLink={data.googleMapsUrl}
+        title={data.title}
+        dates={data.startDate + " - " + data.endDate}
+        text={data.description}
       />
+      })}
+      
     </div>
   );
 }
